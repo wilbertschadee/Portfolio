0ae055b34d08aa578308448d4e2656112291bdf3
wilbert <wilbert_schadee_91@hotmail.com>
2018-11-13 16:13:50 +0100

    veranderingen in de stijl.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function myFunction() {
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
 
+
+  
   var scaledValue = [scaleValue(scrolled, 3, 8, 1, 0), scaleValue(scrolled, 23, 40, 0, 1), scaleValue(scrolled, 43, 50, 1, 0), scaleValue(scrolled, 69, 79, 0, 1), scaleValue(scrolled, 82, 93, 1, 0)];
 
   var foto = [document.getElementById("header"), document.getElementById("img2"), document.getElementById("img3")];
@@ -21,7 +23,13 @@ function myFunction() {
     foto[2].style.opacity = scaledValue[4];
   }
 
-  console.log(scrolled)
+  
+  const headerRect = fot0[0].getBoundingClientRect();
+  
+  console.log(headerRect);
+
+  
+  // console.log(scrolled)
 
   // document.getElementById("myBar").style.height = scrolled + "%";
 }
