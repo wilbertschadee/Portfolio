05d2f5db0145a1c5b8628d1715f7bf38c09ad040
wilbert <wilbert_schadee_91@hotmail.com>
2018-11-14 16:15:32 +0100

    beautify

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-window.onscroll = function() { myFunction() };
+window.onscroll = function () {
+  myFunction()
+};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
 
-
-  
-  var scaledValue = [scaleValue(scrolled, 3, 8, 1, 0), 
+  var scaledValue = [scaleValue(scrolled, 3, 8, 1, 0),
     // scaleValue(scrolled, 23, 40, 0, 1), 
     // scaleValue(scrolled, 43, 50, 1, 0), 
     // scaleValue(scrolled, 69, 79, 0, 1), 
@@ -22,7 +22,7 @@ function myFunction() {
     // scaleValue(scrolled, 82, 93, 1, 0),
   ];
 
-  var foto = [document.getElementById("header"), 
+  var foto = [document.getElementById("header"),
     // document.getElementById("img-1"), 
     // document.getElementById("img-2"),
     // document.getElementById("img-3"),
@@ -34,22 +34,20 @@ function myFunction() {
 
   if (scrolled < 20) {
     foto[0].style.opacity = scaledValue[0];
-  // } else if (scrolled < 41) {
-  //   foto[1].style.opacity = scaledValue[1];
-  // } else if (scrolled > 20 && scrolled < 65) {
-  //   foto[1].style.opacity = scaledValue[2];
-  // } else if (scrolled > 58 && scrolled < 82) {
-  //   foto[2].style.opacity = scaledValue[3];
-  // } else if (scrolled > 83 && scrolled < 100) {
-  //   foto[2].style.opacity = scaledValue[4];
+    // } else if (scrolled < 41) {
+    //   foto[1].style.opacity = scaledValue[1];
+    // } else if (scrolled > 20 && scrolled < 65) {
+    //   foto[1].style.opacity = scaledValue[2];
+    // } else if (scrolled > 58 && scrolled < 82) {
+    //   foto[2].style.opacity = scaledValue[3];
+    // } else if (scrolled > 83 && scrolled < 100) {
+    //   foto[2].style.opacity = scaledValue[4];
   }
 
-  
   // const headerRect = fot0[0].getBoundingClientRect();
-  
+
   // console.log(headerRect);
 
-  
   // console.log(scrolled)
 
   // document.getElementById("myBar").style.height = scrolled + "%";
@@ -60,4 +58,3 @@ function scaleValue(input, imin, imax, omin, omax) {
   var outputX = percent * (omax - omin) + omin;
   return outputX;
 }
-
